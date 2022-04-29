4cda6298a56e90f36f7d5b8936ccb4d07a4712df
sadomaru22 <chilling22sado@gmail.com>
2022-04-29 19:52:48 +0900

    NotFound.tsxの作成とそれに伴うディレクトリの追加

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import { Helmet, HelmetProvider } from 'react-helmet-async';
+//import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
