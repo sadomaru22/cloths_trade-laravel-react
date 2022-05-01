4b9c5b038b2865a24c0564a84679a0eb5cff54c2
sadomaru22 <chilling22sado@gmail.com>
2022-05-01 12:26:22 +0900

    redux, redux-tool-kit導入。 準備。(dispatchなど)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from 'store';
 //import { Helmet, HelmetProvider } from 'react-helmet-async';
 //import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
       <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
