7fbf53a2aa3e66e3592c8eda92563bf9794a0404
sadomaru22 <chilling22sado@gmail.com>
2022-05-06 21:04:02 +0900

    .envファイルの追加 環境変数を宣言→エラー解消

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -5,11 +5,15 @@ import NotFound from 'pages/error/NotFound'
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
+
+
 const Routes = () => {
    return (
       <Switch>
       {/* `exact`を付与しないと`/`以外のパスも含まれる */}
        <Route exact path='/' component={Home} />
+
+       <Route exact path='/login' component={SignIn} />
       {/* 設定した全てのパスに該当しないアクセスを捕捉 */}
        <Route path='*' component={NotFound} />
      </Switch>
