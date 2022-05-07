bbc3a499d2c9f60342e459514c055c45275b6191
sadomaru22 <chilling22sado@gmail.com>
2022-05-07 20:33:25 +0900

    DefaultRouteの設定(Route.tsx)

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,9 +1,29 @@
-import Home from 'pages'
-import SignIn from 'pages/auth/SignIn'
-import NotFound from 'pages/error/NotFound'
-//import Home from 'pages/Home'
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { useEffect } from 'react';
+
+import { Switch, Route as DefaultRoute, Redirect } from 'react-router-dom';
+
+import Home from './pages';
+// import { Privacy, Terms } from './pages/static';
+// import {
+//   SignUp,
+//   EmailVerification,
+//   SignIn,
+//   Account,
+//   ForgotPassword,
+//   ResetPassword,
+// } from './pages/auth';
+//import * as TaskBoard from 'pages/boards';
+import NotFound from 'pages/error/NotFound';
+import { setFlash } from './store/slices/authSlice';
+import { useAppDispatch, useAppSelector, useQuery } from './utils/hooks';
+import SignIn from 'pages/auth/SignIn';
+//import { isAfterRegistration, isSignedIn } from './utils/auth';
+
+//useAppSelectorでstoreのstateにアクセス。notFound=notFoundになれば全て<NoyFound/>へ遷移させる
+const Route = ({ ...rest }) => {
+   const notFound = useAppSelector((state) => state.app.notFound);
+   return notFound ? <NotFound /> : <DefaultRoute {...rest} />;
+ };
 
 
 
