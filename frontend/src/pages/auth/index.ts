f3d99692605f78d2eec873558fb34f70cbc4472b
sadomaru22 <chilling22sado@gmail.com>
2022-05-09 21:09:11 +0900

    Account.tsxの作成 components/accountの追加 moment(日時操作)のインストール

--- frontend/src/pages/auth/index.ts
+++ frontend/src/pages/auth/index.ts
@@ -3,5 +3,6 @@ export { default as SignUp } from './SignUp';
 export { default as ForgotPassword } from './ForgotPassword';
 export { default as ResetPassword } from './ResetPassword';
 export { default as EmailVerification } from './EmailVerification';
+export { default as Account } from './Account';
 
 
